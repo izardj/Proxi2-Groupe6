1d126b60822f1fa760eaf48a9356003decac66b9
Stagiaire <Stagiaire@192.168.1.176>
2017-04-11 14:57:03 +0200

    ajout des getter and setter de Personne dans la classe fille Client

--- ProxiV2-AF-IJ/src/metier/Client.java
+++ ProxiV2-AF-IJ/src/metier/Client.java
@@ -58,6 +58,115 @@ public abstract class Client extends Personne {
 	public void setConseiller(Conseiller conseiller) {
 		this.conseiller = conseiller;
 	}
+	
+
+	@Override
+	protected String getNom() {
+		// TODO Auto-generated method stub
+		return super.getNom();
+	}
+
+	@Override
+	protected void setNom(String nom) {
+		// TODO Auto-generated method stub
+		super.setNom(nom);
+	}
+
+	@Override
+	protected String getPrenom() {
+		// TODO Auto-generated method stub
+		return super.getPrenom();
+	}
+
+	@Override
+	protected void setPrenom(String prenom) {
+		// TODO Auto-generated method stub
+		super.setPrenom(prenom);
+	}
+
+	@Override
+	protected String getAdresse() {
+		// TODO Auto-generated method stub
+		return super.getAdresse();
+	}
+
+	@Override
+	protected void setAdresse(String adresse) {
+		// TODO Auto-generated method stub
+		super.setAdresse(adresse);
+	}
+
+	@Override
+	protected String getCodePostal() {
+		// TODO Auto-generated method stub
+		return super.getCodePostal();
+	}
+
+	@Override
+	protected void setCodePostal(String codePostal) {
+		// TODO Auto-generated method stub
+		super.setCodePostal(codePostal);
+	}
+
+	@Override
+	protected String getVille() {
+		// TODO Auto-generated method stub
+		return super.getVille();
+	}
+
+	@Override
+	protected void setVille(String ville) {
+		// TODO Auto-generated method stub
+		super.setVille(ville);
+	}
+
+	@Override
+	protected String getTelephone() {
+		// TODO Auto-generated method stub
+		return super.getTelephone();
+	}
+
+	@Override
+	protected void setTelephone(String telephone) {
+		// TODO Auto-generated method stub
+		super.setTelephone(telephone);
+	}
+
+	@Override
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return super.getEmail();
+	}
+
+	@Override
+	public void setEmail(String email) {
+		// TODO Auto-generated method stub
+		super.setEmail(email);
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
 
 	@Override
 	public String toString() {
