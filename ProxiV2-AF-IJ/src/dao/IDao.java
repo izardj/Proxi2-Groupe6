f41374ff7f01954c6d5b650caa0e530fa02f8a61
Stagiaire <Stagiaire@192.168.1.176>
2017-04-11 16:08:00 +0200

    1 seul interface dao

--- ProxiV2-AF-IJ/src/dao/IDao.java
+++ ProxiV2-AF-IJ/src/dao/IDao.java
@@ -3,11 +3,15 @@ package dao;
 import java.util.Collection;
 
 import metier.Client;
+import metier.Compte;
+import metier.Conseiller;
 
-public interface IDaoClient {
+public interface IDao {
 	
+	public Conseiller verificationLogin(String login, String pwd);
 	public Collection<Client> listerClientParConseiller(int idConseiller);
 	public void modifierClient(Client client);
 	public Client returnClientParId(int IdClient);
+	public boolean virement(Compte compteCred,Compte compteDeb);
 	
 }
--- ProxiV2-AF-IJ/src/dao/IDaoLogin.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import metier.Conseiller;
-
-public interface IDaoLogin {
-	public Conseiller verificationLogin(String login, String pwd);
-
-}
