0aefcce826dbedafe7ea5b30165a897983ab9d3d
Jerome Izard <izard.jerome@free.fr>
2017-04-11 17:56:05 +0200

    Modification de l'interface Dao

--- ProxiV2-AF-IJ/src/dao/IDao.java
+++ ProxiV2-AF-IJ/src/dao/IDao.java
@@ -7,16 +7,33 @@ import metier.Compte;
 import metier.Conseiller;
 
 public interface IDao {
-	
-	public Conseiller verificationLogin(String login, String pwd);
-	public Collection<Client> listerClientParConseiller(int idConseiller);
-	public void modifierClient(Client client);
-	public Client returnClientParId(int IdClient);
-	/**
-	 * Met à jour le solde de compte
-	 * @param compte Le compte à modifier
-	 * @return 1 en cas de succès ou 0 si le compte n'a pas été modifié
-	 */
+
 	public int modifierSoldeCompte(Compte compte);
-	
+
+	public int creerConseiller(Conseiller conseiller);
+
+	public int modifierConseiller(Conseiller conseiller);
+
+	public Conseiller verificationLogin(String login, String pwd);
+
+	public int supprimerConseiller(Conseiller conseiller);
+
+	public int creerCompte(Compte compte);
+
+	public int modifierCompte(Compte compte);
+
+	public Compte getCompteParId(int id);
+
+	public int supprimerCompte(Compte compte);
+
+	public int creerClient(Client client);
+
+	public int modifierClient(Client client);
+
+	public int supprimerClient(Client client);
+
+	public Client retourneClientParId(int IdClient);
+
+	public Collection<Client> listerClientsParConseiller(int idConseiller);
+
 }
