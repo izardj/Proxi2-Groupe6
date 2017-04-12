5592b9eb2a5135a19683ac15c7d5e91d1c6d4a28
Jerome Izard <izard.jerome@free.fr>
2017-04-12 16:34:13 +0200

    Suite de test

--- ProxiV2-AF-IJ/src/tests/AllTests.java
+++ ProxiV2-AF-IJ/src/tests/AllTests.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ServicesTest.class })
+@SuiteClasses({ DaoTest.class, ServicesTest.class })
+
 public class AllTests {
 
 }
