bab643de7bd390df08c7f84af1eab2c4f862646d
fayraud <florent.ayraud@gmail.com>
2017-04-12 13:51:15 +0200

    javacdoc ILoginService

--- ProxiV2-AF-IJ/src/service/ILoginService.java
+++ ProxiV2-AF-IJ/src/service/ILoginService.java
@@ -3,7 +3,12 @@ package service;
 import metier.Conseiller;
 
 public interface ILoginService {
-
+	/**
+	 * Methode d'authentification par login et PWD		
+	 * @param login parametre login	
+	 * @param pwd parametr  mots de passe
+	 * @return
+	 */
 	public Conseiller verificationLogin(String login, String pwd);
 
 }
